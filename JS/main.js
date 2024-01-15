bc0eebb3adc9cc7f0829436b0ef606935ea0768c
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-15 17:54:19 +0100

    add: addEventListeners and ciclo for

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
 // ELEMENTI
 const image = document.getElementById('image')
+const upArrow = document.getElementById('up-arrow')
+const downArrow = document.getElementById('down-button')
 
 // al caricamento della pagina generare la lista di slides rendendo visibile solo la prima
 
@@ -13,5 +15,30 @@ const slides = [
 
 console.log(slides[0])
 
-image.innerHTML=('<img id="image" src="./img/01.webp" alt="image">')
+image.innerHTML = ('<img id="image" src="./img/01.webp" alt="image">')
+console.log(image.innerHTML)
+
+
+// al click della freccia in alto
+
+upArrow.addEventListener('click', function () {
+
+    // visualizzare l'immagine succesiva in base all'ordine dell'array
+
+    for (let i = 0; i <= slides.length - 1; i++) {
+        console.log([i])
+
+        const cerca =  (`src="./img/${slides[i]}"`)
+
+        console.log(cerca)
+        console.log(slides[i])
+
+        image.innerHTML = (`<img id="image" ${cerca} alt="image">`)
+        
+
+
+    }
+
+})
+
 
